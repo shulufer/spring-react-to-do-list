e2e6ef0d3fbbffe0d43e82e4a24b94f4fd10abf0
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-11-13 13:20:16 +0100

    password save was with encode

--- ws/src/main/java/com/todolist/ws/user/UserService.java
+++ ws/src/main/java/com/todolist/ws/user/UserService.java
@@ -1,6 +1,8 @@
 package com.todolist.ws.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,7 +11,11 @@ public class UserService {
   @Autowired
   UserRepository userRepository;
 
+  PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
   public void save(User user) {
+    String encodedPassword = passwordEncoder.encode(user.getPassword());
+    user.setPassword(encodedPassword);
     userRepository.save(user);
   }
 

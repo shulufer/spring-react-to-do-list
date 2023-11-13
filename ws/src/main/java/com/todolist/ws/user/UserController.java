6625171423eb9ca21adcd5b6b32db74a2d37678b
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-11-13 10:37:42 +0100

    proxy has been set

--- ws/src/main/java/com/todolist/ws/user/UserController.java
+++ ws/src/main/java/com/todolist/ws/user/UserController.java
@@ -1,13 +1,12 @@
 package com.todolist.ws.user;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 public class UserController {
 
-  @CrossOrigin
+
   @PostMapping(value = "/api/v1/users")
   public void createUser() {
 

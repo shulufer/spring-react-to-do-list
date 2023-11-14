7e7855d080349f81f5f99aebbb6afa71a09e16c8
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-11-14 17:18:35 +0100

    The user is informed about ongoing transactions (successful scenarios)

--- frontend/src/pages/SignUp/index.jsx
+++ frontend/src/pages/SignUp/index.jsx
@@ -7,14 +7,21 @@ export function SignUp() {
   const [email, setEmail] = useState()
   const [password, setPassword] = useState()
   const [passwordRepeat, setPasswordRepeat] = useState()
+  const [apiProgress, setApiProgress] = useState(false); // baslangic durumunda false
+  const [successMessage, setSuccessMessage] = useState(); // kullaniya mesaj gostermek icin
+
 
   const onSubmit = (event) => {
     event.preventDefault();
+    setApiProgress(true); // form submit edildiginde bu trueya cekildi.
     axios.post('/api/v1/users', {
       username,
       email,
       password
+    }).then((response) => { //reponsemiz sonrasinda msji alip successMessage olarak kayit ediyoruz.
+      setSuccessMessage(response.data.message)
     })
+    .finally(() => setApiProgress(false)) //loading kapatmak icin request islemi bitince apiProgresi true yapiyoruz.
   }
 
   return (
@@ -41,7 +48,16 @@ export function SignUp() {
             <label htmlFor="passwordRepeat" className="form-label">Password Repeat: </label>
             <input id="passwordRepeat" className="form-control" type="password" onChange={(event) => setPasswordRepeat(event.target.value)} />
           </div>
-          <button className="btn btn-primary" disabled={!password || password !== passwordRepeat || !username || !email}>Sign Up</button>
+          <div>
+            {successMessage && <div className="alert alert-secondary" role="alert"> {successMessage} </div>}
+            {/* kullanilci yaratilmissa msj gostermesi icin. successmessage var ise goster. backenden gelen responeseyi isleyip gosteriyoruz. */}
+          </div>
+          <button className="btn btn-primary" disabled={apiProgress || (!password || password !== passwordRepeat || !username || !email)}>
+              {apiProgress && <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>}
+              {/* Ekrana loading gozukmesi icin yaptik. api progres true ise sag blok calisicak anlamina geliyor. */}
+            Sign Up
+          </button>
+          {/* butona ekledikform submit olursa disadble olsun diye */}
         </div>
       </form>
     </div>
